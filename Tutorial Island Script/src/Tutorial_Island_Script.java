b28245047b0899b6a2cc29656172f6cc9d8eb052
TJohnsonSE <tylerjohnsonSE@outlook.com>
2022-08-31 20:15:20 -0700

    Minimal change; still working on first room

--- Tutorial Island Script/src/Tutorial_Island_Script.java
+++ Tutorial Island Script/src/Tutorial_Island_Script.java
@@ -6,6 +6,9 @@ import com.epicbot.api.shared.script.LoopScript;
 import com.epicbot.api.shared.script.ScriptManifest;
 import com.epicbot.api.shared.util.paint.frame.PaintFrame;
 import com.epicbot.api.shared.webwalking.model.RSBank;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
+import javafx.geometry.Rectangle2D;
 
 import com.epicbot.api.shared.entity.*;
 import com.epicbot.api.shared.methods.IBankAPI;
@@ -19,6 +22,7 @@ import com.epicbot.api.shared.methods.IGroundItemsAPI;
 import com.epicbot.api.shared.methods.IInventoryAPI;
 import com.epicbot.api.shared.methods.ILocalPlayerAPI;
 import com.epicbot.api.shared.methods.IMagicAPI;
+import com.epicbot.api.shared.methods.IMouseAPI;
 import com.epicbot.api.shared.methods.INPCsAPI;
 import com.epicbot.api.shared.methods.IObjectsAPI;
 import com.epicbot.api.shared.methods.ISkillsAPI;
@@ -36,112 +40,118 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
+import org.apache.xmlgraphics.java2d.Graphics2DImagePainter;
+
 @ScriptManifest(name = "Tutorial Island Completer by CloudSE", gameType = GameType.OS)
 
 public class Tutorial_Island_Script extends LoopScript {
 
-    public final Boolean DEBUG = true;
-    public Timer timer = new Timer(60);
-    public String status = "";
-    public int experienceGained = 0;
-    public Boolean gielenorGuideDone = false;
-    public DiscordMessage discordMessage;
-    public DiscordMessageBuilder messageBuilder;
+	public final Boolean DEBUG = true;
+	public Timer timer = new Timer(60);
+	public String status = "";
+	public int experienceGained = 0;
+	public Boolean gielenorGuideDone = false;
+	public DiscordMessage discordMessage;
+	public DiscordMessageBuilder messageBuilder;
 
-    /*
-     * // The current local player, meaning you; returns the API reference to your
-     * // player character
-     */
-    public ILocalPlayerAPI myPlayer() {
+	/*
+	 * // The current local player, meaning you; returns the API reference to your
+	 * // player character
+	 */
+	public ILocalPlayerAPI myPlayer() {
 
-        return getAPIContext().localPlayer();
-    }
+		return getAPIContext().localPlayer();
+	}
 
-    public ICameraAPI globalCamera() {
-        return getAPIContext().camera();
-    }
+	public ICameraAPI globalCamera() {
+		return getAPIContext().camera();
+	}
 
-    public IGameAPI game() {
-        return getAPIContext().game();
-    }
+	public IGameAPI game() {
+		return getAPIContext().game();
+	}
 
-    /*
-     * A reference to all the objects in the scene
-     */
-    public IObjectsAPI sceneObjects() {
-        return getAPIContext().objects();
-    }
+	/*
+	 * A reference to all the objects in the scene
+	 */
+	public IObjectsAPI sceneObjects() {
+		return getAPIContext().objects();
+	}
 
-    /*
-     * // The current items lying on the ground; retruns the API reference to all
-     * items
-     * // lying on the ground
-     */
-    public IGroundItemsAPI groundItems() {
-        return getAPIContext().groundItems();
-    }
+	/*
+	 * // The current items lying on the ground; retruns the API reference to all
+	 * items
+	 * // lying on the ground
+	 */
+	public IGroundItemsAPI groundItems() {
+		return getAPIContext().groundItems();
+	}
 
-    // Your inventory; returns the API reference to your inventory
-    public IInventoryAPI myInventory() {
+	// Your inventory; returns the API reference to your inventory
+	public IInventoryAPI myInventory() {
 
-        return getAPIContext().inventory();
-    }
+		return getAPIContext().inventory();
+	}
 
-    // Your bank; returns the API reference to your bank
-    public IBankAPI myBank() {
-        return getAPIContext().bank();
-    }
+	// Your bank; returns the API reference to your bank
+	public IBankAPI myBank() {
+		return getAPIContext().bank();
+	}
 
-    // A variable that utilizes several methods to move the player character around
-    public IWebWalkingAPI walk() {
+	// A variable that utilizes several methods to move the player character around
+	public IWebWalkingAPI walk() {
 
-        return getAPIContext().webWalking();
-    }
+		return getAPIContext().webWalking();
+	}
 
-    public IEquipmentAPI equipment() {
-        return getAPIContext().equipment();
-    }
+	public IEquipmentAPI equipment() {
+		return getAPIContext().equipment();
+	}
 
-    public ITabsAPI tabs() {
-        return getAPIContext().tabs();
-    }
+	public ITabsAPI tabs() {
+		return getAPIContext().tabs();
+	}
 
-    public IMagicAPI magic() {
-        return getAPIContext().magic();
-    }
+	public IMagicAPI magic() {
+		return getAPIContext().magic();
+	}
 
-    public IDiscordAPI discord() {
-        return getAPIContext().discord();
-    }
+	public IMouseAPI mouse() {
+		return getAPIContext().mouse();
+	}
 
-    public IGrandExchangeAPI grandExchange() {
-        return getAPIContext().grandExchange();
-    }
+	public IDiscordAPI discord() {
+		return getAPIContext().discord();
+	}
 
-    public IDialogueAPI dialogue() {
-        return getAPIContext().dialogues();
-    }
+	public IGrandExchangeAPI grandExchange() {
+		return getAPIContext().grandExchange();
+	}
 
-    public ISkillsAPI playerSkills() {
-        return getAPIContext().skills();
-    }
+	public IDialogueAPI dialogue() {
+		return getAPIContext().dialogues();
+	}
 
-    /*
-     * API to interact with NPC's around the player character
-     */
-    public INPCsAPI npcs() {
-        return getAPIContext().npcs();
-    }
+	public ISkillsAPI playerSkills() {
+		return getAPIContext().skills();
+	}
 
-    // End API variables
+	/*
+	 * API to interact with NPC's around the player character
+	 */
+	public INPCsAPI npcs() {
+		return getAPIContext().npcs();
+	}
 
-    // --------------------
+	// End API variables
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
+	// --------------------
 
-    public Boolean completeGielinorGuide(List<NPC> npcsList) {
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Boolean completeGielinorGuide(List<NPC> npcsList) {
 
         if (DEBUG) {
             System.out.println(npcsList.toString());
@@ -151,43 +161,47 @@ public class Tutorial_Island_Script extends LoopScript {
 
         if (gielinorGuide.canReach(getAPIContext())) {
 
-            gielinorGuide.click();
+            gielinorGuide.click(); //Click on the guide
+			
+			
+
         }
 
-        return true;
-    }
+	return true;
 
-    @Override
+	}
+
+	@Override
     public boolean onStart(String... strings) {
         setStatus("Script starting");
 
         return true;
     }
 
-    @Override
-    protected int loop() {
+	@Override
+	protected int loop() {
 
-        List<NPC> npcsList = npcs().getAll();
+		List<NPC> npcsList = npcs().getAll();
 
-        if (gielenorGuideDone == false) {
-            setStatus("Gielenor guide");
+		if (gielenorGuideDone == false) {
+			setStatus("Gielenor guide");
 
-            if (completeGielinorGuide(npcsList)) {
+			if (completeGielinorGuide(npcsList)) {
 
-                gielenorGuideDone = true;
-            }
-        }
+				gielenorGuideDone = true;
+			}
+		}
 
-        return 50;
-    }
+		return 50;
+	}
 
-    @Override
-    protected void onPaint(Graphics2D g, APIContext ctx) {
-        PaintFrame frame = new PaintFrame("High Alcher by CloudSE");
-        frame.addLine("Script run-time: ", Time.getFormattedTime(timer.getElapsed()));
-        frame.addLine("Action being performed: ", status);
-        frame.addLine("Experience gained: ", experienceGained);
-        frame.draw(g, 0, 0, ctx);
-    }
+	@Override
+	protected void onPaint(Graphics2D g, APIContext ctx) {
+		PaintFrame frame = new PaintFrame("High Alcher by CloudSE");
+		frame.addLine("Script run-time: ", Time.getFormattedTime(timer.getElapsed()));
+		frame.addLine("Action being performed: ", status);
+		frame.addLine("Experience gained: ", experienceGained);
+		frame.draw(g, 0, 0, ctx);
+	}
 
 }
